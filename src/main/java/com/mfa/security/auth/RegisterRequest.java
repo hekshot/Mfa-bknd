1037b64dbe1ebb019f5fca72462ae18a46e89e82
hekshot <amankumarkr111@gmail.com>
2023-11-07 14:03:26 +0530

    first commit

---
+++ src/main/java/com/mfa/security/auth/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.mfa.security.auth;
+
+import com.mfa.security.user.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+  
+  private String firstname;
+  private String lastname;
+  private String email;
+  private String password;
+  private Role role;
+  private boolean mfaEnabled;
+}
