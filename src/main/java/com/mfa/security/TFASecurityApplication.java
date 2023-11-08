a050b857b5fe9634de4914982b9c11f4a5870635
hekshot <amankumarkr111@gmail.com>
2023-11-08 15:20:24 +0530

    for tomcat10

--- src/main/java/com/mfa/security/TFASecurityApplication.java
+++ src/main/java/com/mfa/security/TFASecurityApplication.java
@@ -2,11 +2,20 @@ package com.mfa.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TFASecurityApplication {
+public class TFASecurityApplication extends SpringBootServletInitializer {
 
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+		return applicationBuilder.sources(TFASecurityApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(TFASecurityApplication.class, args);
 	}
+	
+
 }
